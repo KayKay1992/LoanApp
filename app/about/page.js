7ac8bdd77ad288dc6302fa5f78c25f921ef697b9
KayKay1992 <kennethnwankpa1992@gmail.com>
2023-09-01 16:19:21 -0700

    About page with Navbar

--- app/about/page.js
+++ app/about/page.js
@@ -8,21 +8,21 @@ export default function page() {
       </h1>
       <br />
       <h2 className="text-center font-bold bg-transparent">Our Vision</h2>
-      <div className="flex sm: .flex-col">
+      <div className="sm:flex ml-7">
         <div className="max-w-xl mr-10">
-          <p>
+          <p className="mb-7">
             Quickloan is the definitive bank for small and medium-sized
             businesses in Nigeria and is regulated by the Central Bank of
             Nigeria (CBN). Quickloan MFB was also awarded by the CBN in 2022 as
             the most inclusive payment platform in the country.
           </p>
-          <p>
+          <p className="mb-7">
             Quickloan MFB received its license in February 2022 and helps over
             1.5 million businesses with banking, payments processing, access to
             loans and business management tools. With these solutions,
             businesses on the platform process about 8 trillion Naira monthly.
           </p>
-          <p>
+          <p className="mb-7">
             Quickloan operates the largest distribution network for financial
             services in Nigeria, and over 33 million people use their cards on
             our POS terminals monthly across every local government in Nigeria.
